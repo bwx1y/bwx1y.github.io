5da585b9b908bf1ee994318cbddb7cf0a12e1ce1
bwx1y <nedynugroho2007@gmail.com>
2022-04-16 16:04:33 -0700

    mnambahkan pertanyaan

--- scr/js/script-animasi.js
+++ scr/js/script-animasi.js
@@ -1,3 +1,8 @@
+// AOS aimation
+AOS.init({
+  duration: 1000
+});
+
 // GSAP
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
@@ -6,60 +11,3 @@ gsap.from(".jumbotron img", { duration: 1, opacity: 0, delay: 0.8 });
 gsap.from("#about h2", { duration: 1, opacity: 0 });
 gsap.to(".jumbotron h1", { duration: 1.5, text: "Nedy Nugroho", delay: 1.1 });
 gsap.to(".jumbotron p", { duration: 1.5, text: "I am a student", delay: 1.3 });
-
-// animasi scroll
-gsap.to(".AnimasiText-1", {
-  scrollTrigger: {
-    trigger: ".AnimasiText-1",
-    start: "20px 80%",
-    toggleAction: "restart pause none none",
-  },
-  duration: 2,
-  text: "About Me",
-});
-
-gsap.to(".AnimasiText-2", {
-  scrollTrigger: {
-    trigger: ".AnimasiText-2",
-    start: "20px 80%",
-    toggleAction: "restart pause none none",
-  },
-  duration: 2,
-  text: "My Project",
-});
-
-gsap.from(".AnimasiText-3", {
-  scrollTrigger: {
-    trigger: ".AnimasiText-3",
-    start: "20px 80%",
-    toggleAction: "restart pause none none",
-  },
-  duration: 2,
-  x: -100,
-  opacity: 0,
-  delay: 0.5,
-});
-
-gsap.from(".AnimasiText-4", {
-  scrollTrigger: {
-    trigger: ".AnimasiText-4",
-    start: "20px 80%",
-    toggleAction: "restart pause none none",
-  },
-  duration: 2,
-  x: 100,
-  opacity: 0,
-  delay: 0.6,
-});
-
-gsap.from("#project .card", {
-  scrollTrigger: {
-    trigger: "#project .card",
-    start: "20px 80%",
-    toggleAction: "restart pause none start",
-  },
-  duration: 2.5,
-  x: 0,
-  opacity: 0,
-  delay: 0.2,
-});
